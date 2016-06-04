35a99ad84950935ee2feff7ff0bcaed3ff6c64b0
Sergio Mendoza <sm8a76@gmail.com>
2016-06-03 20:34:15 -0500

    Added Grocery, Supermarket, Quote and List to the boot script

--- server/boot/script.js
+++ server/boot/script.js
@@ -7,7 +7,8 @@ module.exports = function(app) {
 
        Customer.create([
         {username: 'admin', email: 'sm8a76@gmail.com', password: 'abcdef'},
-        {username: 'smendoza', email: 'sm8a76@gmail.com', password: 'abcdef'}
+        {username: 'smendoza', email: 'sm8a76@gmail.com', password: 'abcdef'},
+        {username: 'jsmith', email: 'sm8a76@gmail.com', password: 'abcdef'}
       ], function(err, users) {
         if (err) return cb(err);
         
@@ -42,9 +43,129 @@ module.exports = function(app) {
       ], function(err, supermarkets) {
         if (err) throw (err);
         console.log('Supermarket created and populated.');
+        //console.log(supermarkets);
       });
-    });    
-  
+    });  
+    
+    MongoDB.automigrate('Grocery', function(err) {
+       if (err) throw (err);
+       var Grocery = app.models.Grocery;
+
+       Grocery.create([
+        {name: 'Leche Entera Santa Clara', description: 'Leche Entera Santa Clara', image: 'images/leche-entera-santaclara.png', unit: 'Litros', madeBy: 'Santa Clara'},
+        {name: 'Leche Entera Lala', description: 'Leche Entera Lala', image: 'images/leche-entera-lala.jpeg', unit: 'Litros', madeBy: 'Lala'},           
+        {name: 'Leche Deslactosada Santa Clara', description: 'Leche Deslactosada Santa Clara', image: 'images/leche-deslactosada-santaclara.jpg', unit: 'Litros', madeBy: 'Santa Clara'},                           {name: 'Leche Deslactosada Parmalat', description: 'Leche Deslactosada Parmalat', image: 'images/leche-deslactosada-parmalat.jpg', unit: 'Litros', madeBy: 'Parmalat'}
+      ], function(err, groceries) {
+        if (err) throw (err);
+        console.log('Groceries created and populated.');
+        //console.log(groceries);
+      });
+    });        
+    
+    MongoDB.automigrate('Quote', function(err) {
+       if (err) throw (err);
+       var Quote = app.models.Quote;
+       var Grocery = app.models.Grocery;
+       var Supermarket = app.models.Supermarket;
+        
+        var groceryId, hebId, walmartId, sorianaId;
+        
+        Supermarket.find({name: 'HEB Puerta de Hierro'}, function(err, supermarket){
+            hebId = supermarket.id;
+        }); 
+        
+        Supermarket.find({name: 'Soriana Lincoln'}, function(err, supermarket){
+            sorianaId = supermarket.id;
+        });         
+        
+        Supermarket.find({name: 'Walmart Cumbres'}, function(err, supermarket){
+            walmartId = supermarket.id;
+        });           
+
+      //***** 
+        Grocery.find({name: 'Leche Entera Santa Clara'}, function(err, grocery){
+            groceryId = grocery.id;
+        });        
+        
+       Quote.create([
+        {price: 1550, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: hebId},
+        {price: 1650, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: sorianaId},
+        {price: 1750, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: walmartId}
+      ], function(err, quotes) {
+        if (err) throw (err);
+        console.log('Quotes 1 created and populated.');
+      });
+        
+        
+        //*****
+        Grocery.find({name: 'Leche Entera Lala'}, function(err, grocery){
+            groceryId = grocery.id;
+        });        
+        
+       Quote.create([
+        {price: 1550, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: hebId},
+        {price: 1450, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: sorianaId},
+        {price: 1350, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: walmartId}
+      ], function(err, quotes) {
+        if (err) throw (err);
+        console.log('Quotes 2 created and populated.');
+      });
+        
+        //*****
+        Grocery.find({name: 'Leche Deslactosada Santa Clara'}, function(err, grocery){
+            groceryId = grocery.id;
+        });        
+        
+       Quote.create([
+        {price: 1750, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: hebId},
+        {price: 1850, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: sorianaId},
+        {price: 1950, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: walmartId}
+      ], function(err, quotes) {
+        if (err) throw (err);
+        console.log('Quotes 3 created and populated.');
+      });   
+        
+        
+        //*****
+        Grocery.find({name: 'Leche Deslactosada Parmalat'}, function(err, grocery){
+            groceryId = grocery.id;
+        });        
+        
+       Quote.create([
+        {price: 1950, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: hebId},
+        {price: 2050, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: sorianaId},
+        {price: 1850, unit: 'Litro', promo: '', discountIfAny: 0, groceryId: groceryId, superId: walmartId}
+      ], function(err, quotes) {
+        if (err) throw (err);
+        console.log('Quotes 4 created and populated.');
+      });          
+        
+        
+        
+    });     
+    
+    
+    MongoDB.automigrate('List', function(err) {
+       if (err) throw (err);
+       var List = app.models.List;
+       var Customer = app.models.Customer;
+        
+        var customerId;
+        
+        Customer.find({username: 'jsmith'}, function(err, customer){
+            customerId = customer.id;
+        });              
+        
+
+       List.create([
+        {name: 'MyWeeklyList', customerId: customerId},
+        {name: 'MyMonthlyList', customerId: customerId},
+        {name: 'Other', customerId: customerId}
+      ], function(err, lists) {
+        if (err) throw (err);
+        console.log('Lists created and populated for jsmith.');
+      });
+    });       
     
     
 };
